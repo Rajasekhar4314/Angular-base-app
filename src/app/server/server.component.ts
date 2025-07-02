d6d8013e88e70fa33122e61d783dcf8f1ada277b
Rajasekhar4314 <rajasekhar4314@gmail.com>
2025-07-03 01:09:03 +0530

    Added Interpolation and property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component} from "@angular/core"
 })
 
 export class ServerComponent {
+    serverId = 10;
+    serviceStatus = false
 
+    getServiceStatus(){
+        return this.serviceStatus
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = "No server was created"
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server is created"
+  }
+
+
 
 }
