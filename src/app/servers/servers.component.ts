aebc334e482697eafd099d103c6e45b52d1ac3a8
Rajasekhar4314 <rajasekhar4314@gmail.com>
2025-07-05 01:40:33 +0530

    Directives: ngIf & ngStyles added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -12,11 +12,17 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = "No server was created"
   serverName: String = 'Gcp mc'
   username: String = '';
+  accountCreated: boolean = false;
+  accountName: String = ''
+  accountStatus: String = ''
+
 
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000)
+
+    this.accountStatus = Math.random() > 0.5 ? 'online' : 'offline'
   }
 
   ngOnInit(): void {
@@ -33,7 +39,21 @@ export class ServersComponent implements OnInit {
 
   //  Assignment
   resetUserName(){
-    console.log("i am inside")
     this.username = ''
   }
+
+  createAccount(){
+    this.accountCreated = true;
+
+    // this.accountName = "Account created created successfully, name is "+ this.accountName
+  }
+
+  getAccountStatus(){
+    return this.accountStatus;
+  }
+
+  getColor(){
+    return this.accountStatus === "online" ? "green" : "red";
+  }
+
 }
